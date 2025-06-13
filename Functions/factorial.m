b09f81e88cf5099e8df5096ed9b3592f422a0c7d
Hansamali-Bopeththa <hansamalib002@gmail.com>
2025-06-13 21:54:26 +0530

    Add files via upload

---
+++ Functions/factorial.m
@@ -0,0 +1,13 @@
+
+function fact = factorial(n)
+    if n < 0
+        error('Factorial is undefined for negative numbers.');
+    else
+        fact = 1; % Initialize factorial value
+        for i = 1:n
+            fact = fact * i;
+        end
+    end
+    disp(['Factorial of ', num2str(n), ' is ', num2str(fact)]);
+end
+
